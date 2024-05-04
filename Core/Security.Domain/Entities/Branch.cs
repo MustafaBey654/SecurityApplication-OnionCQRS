a11332adaaabe42146c0ebcacddf28926df63e83
Mustafa Yılmaz <100315730+MustafaBey654@users.noreply.github.com>
2024-05-04 23:49:33 +0300

    Base Entity tables created

---
+++ Core/Security.Domain/Entities/Branch.cs
@@ -0,0 +1,16 @@
+﻿using Security.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Security.Domain.Entities
+{
+    public class Branch:EntityBase,IEntityBase
+    {
+        public string BranchName {  get; set; }
+        public ICollection<Unit> Units { get; set; }
+        
+    }
+}
---
+++ Core/Security.Domain/Entities/Personel.cs
@@ -0,0 +1,16 @@
+﻿using Security.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Security.Domain.Entities
+{
+    public class Personel:EntityBase,IEntityBase
+    {
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public ICollection<Unit> units { get; set; }
+    }
+}
