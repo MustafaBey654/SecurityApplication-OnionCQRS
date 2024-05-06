7166c5585c658c02c72cb743e858b584e7a1fe9e
Mustafa Yılmaz <100315730+MustafaBey654@users.noreply.github.com>
2024-05-06 19:35:39 +0300

    Presentation layer and jwt bearer and context created.

--- Presentation/Security.API/Program.cs
+++ Presentation/Security.API/Program.cs
@@ -1,11 +1,39 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+using Security.Presention;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// presentation katmanýnda controller tanýmlamak için
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(AssemblyReference).Assembly);
+
+
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setup =>
+{
+    var jwtSecuritySheme = new OpenApiSecurityScheme
+    {
+        BearerFormat = "JWT",
+        Name = "JWT Authentication",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Description = "Put ** _only_** your jwt bearer token",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+    setup.AddSecurityDefinition(jwtSecuritySheme.Reference.Id, jwtSecuritySheme);
+    setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {jwtSecuritySheme,Array.Empty<string>() }
+    });
+});
 
 var env = builder.Environment;
 builder.Configuration
